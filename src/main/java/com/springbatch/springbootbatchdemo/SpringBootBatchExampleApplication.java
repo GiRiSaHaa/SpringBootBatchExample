2f5c4cbe9f3bd23d83184f1cf32ad24547d05240
GiRi SaHaa <giri91sa@gmailcom>
2021-04-03 13:33:06 +0530

    initial commit

---
+++ src/main/java/com/springbatch/springbootbatchdemo/SpringBootBatchExampleApplication.java
@@ -0,0 +1,17 @@
+/*
+ * Copyright (c) 2021. By GiRi SaHaa
+ */
+
+package com.springbatch.springbootbatchdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootBatchExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootBatchExampleApplication.class, args);
+	}
+
+}
